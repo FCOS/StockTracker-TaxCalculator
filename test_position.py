820054ea30cfe2efda898f9638b1ea3cc5b9f114
Fergal O Shea <fergalcos@gmail.com>
2020-10-21 21:32:57 +0100

    test

--- test_position.py
+++ test_position.py
@@ -7,7 +7,6 @@ logging.basicConfig(level=logging.INFO)
 shell_holdings = Position(Transaction("RDSA", "08/05/2020", "buy", 220, 12.233))
 print(shell_holdings)
 
-
 shell_holdings.add_transaction(Transaction("RDSA", "08/05/2020", "sell", 190, 13.233))
 print(shell_holdings)
 shell_holdings.add_transaction(Transaction("RDSA", "08/05/2020", "buy", 1, 10))
