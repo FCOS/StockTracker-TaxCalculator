bdb1aad1ec4f59a8bd088ecd61df6d501f4d4b01
Fergal O'Shea <foshea@tcd.ie>
2020-10-21 21:07:29 +0100

    Initial upload
    Position and transaction classes have base functionality
    Portfolio class is unimplemented
    
    The transaction represents a buy or sell of a product. It tracks name, direction (buy or sell), price, amount, whether it's been closed (i.e. you bought but then sold), currency and profit made (on sell orders)
    Position tracks multiple transactions of a common product (i.e. all transactions of Facebook stock), as well as the name, value (cost of purchases, not live yet), amount open, the break-even price, currency and total profit.

---
+++ test_position.py
@@ -0,0 +1,20 @@
+from transaction import Transaction
+from position import Position
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+shell_holdings = Position(Transaction("RDSA", "08/05/2020", "buy", 220, 12.233))
+print(shell_holdings)
+
+
+shell_holdings.add_transaction(Transaction("RDSA", "08/05/2020", "sell", 190, 13.233))
+print(shell_holdings)
+shell_holdings.add_transaction(Transaction("RDSA", "08/05/2020", "buy", 1, 10))
+print(shell_holdings)
+shell_holdings.add_transaction(Transaction("RDSA", "08/05/2020", "buy", 2, 20))
+print(shell_holdings)
+ 
+shell_holdings.print()
+
+#shell_holdings.update_position()
---
+++ test_transaction.py
@@ -0,0 +1,3 @@
+from transaction import Transaction
+
+new_transaction = Transaction("RDSA", "08/05/2020", "Buy", 100, 1)
