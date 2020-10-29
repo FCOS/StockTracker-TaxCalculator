ca10a9e9146d5d1eba07c7b93da366a0fcc8cb83
Fergal O'Shea <fergalo@xlnx.xilinx.com>
2020-10-29 16:41:31 +0000

    Gui enhancemtns, adding positions to display

--- gui.py
+++ gui.py
@@ -1,32 +1,43 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
 from StockTracker import Portfolio
+from datetime import datetime
 
 
 class StockTrackerGui:
 
     def __init__(self, master):
         self.master = master
+        self.portfolio = Portfolio()
+        self.positions = []
         
+        self.clr_main_1 = "#ffffff" #White
+        self.clr_main_2 = "#262626" #Black
+        self.clr_accent_1 = "#d6d6d6" #grey
+        self.clr_accent_2 = "#404040" #grey 2
+
+        self.cost = tk.StringVar()
+        self.cost.set("0")
+
         #Display
         self.master.title("StockTracker")
         self.master.columnconfigure(0, minsize=800, weight=1)
         self.master.rowconfigure(1, minsize=600, weight=1)
 
         # Header frame
-        self.frm_header = tk.Frame(self.master)
-        self.lbl_header_date = tk.Label(self.frm_header, text="28/10/2020", bg="red")
+        self.frm_header = tk.Frame(self.master, bg=self.clr_accent_1)
+        self.lbl_header_date = tk.Label(self.frm_header, text=datetime.date(datetime.now()), bg=self.clr_accent_1)
         self.lbl_header_date.grid(row=0, column=0, sticky="ns", padx=10, pady=10)
 
-        self.lbl_header_value = tk.Label(self.frm_header, text="0", bg="blue")
+        self.lbl_header_value = tk.Label(self.frm_header, textvariable=self.cost, bg=self.clr_accent_1)
         self.lbl_header_value.grid(row=0, column=1, sticky="sewn", padx=10, pady=10)
         self.frm_header.columnconfigure(1, minsize=500, weight=1)
 
         # Body Frame
-        self.frm_body = tk.Frame(self.master, bg="pink")
+        self.frm_body = tk.Frame(self.master, bg=self.clr_main_1)
 
         # Side bar sub frame
-        self.frm_body_side_bar = tk.Frame(self.frm_body, bg="brown")
+        self.frm_body_side_bar = tk.Frame(self.frm_body, bg=self.clr_accent_2)
         self.btn_load_csv = tk.Button(self.frm_body_side_bar, text="Open Portfolio", command=self.load_csv)
         self.btn_enter_manual = tk.Button(self.frm_body_side_bar, text="New Portfolio")
         self.btn_reset = tk.Button(self.frm_body_side_bar, text="Close Portfolio", command=self.close_portfolio)
@@ -36,35 +47,38 @@ class StockTrackerGui:
         self.btn_enter_manual.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
 
         # Main body sub frame
-        self.frm_body_main = tk.Frame(self.frm_body, bg="green")
-        self.lbl_body_main = tk.Label(self.frm_body_main, text="body")
-        self.lbl_body_main.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
+        self.frm_body_main = tk.Frame(self.frm_body, bg=self.clr_main_1)
         self.frm_body_main.rowconfigure(0, weight=1)
         self.frm_body_main.columnconfigure(0, weight=1)
 
-        self.frm_body_side_bar.grid(row=0, column=0, sticky="ns", padx=10, pady=10)
+        self.frm_body_side_bar.grid(row=0, column=0, sticky="ns")
         self.frm_body_main.grid(row=0, column=1, sticky="nesw", padx=10, pady=10)
 
         self.frm_body.columnconfigure(1, weight=1)
         self.frm_body.rowconfigure(0, weight=1)
 
         # Footer frame
-        self.frm_footer = tk.Frame(self.master)
-        self.lbl_footer_author = tk.Label(self.frm_footer, text="Developed by Fergal O'Shea - foshea@tcd.ie", bg="yellow")
+        self.frm_footer = tk.Frame(self.master, bg=self.clr_accent_1)
+        self.lbl_footer_author = tk.Label(self.frm_footer, text="Developed by Fergal O'Shea - foshea@tcd.ie", bg=self.clr_accent_1)
         self.lbl_footer_author.grid(row=0, column=0, sticky="ews", padx=10, pady=1)
 
         # window
         self.frm_header.grid(row=0, column=0, sticky="news")
         self.frm_body.grid(row=1, column=0, sticky="news")
-        self.frm_footer.grid(row=2, column=0)
+        self.frm_footer.grid(row=2, column=0, sticky="news")
 
         #self.reset_gui()
 
     def close_portfolio(self):
-        self.portfolio = None
+        #self.portfolio = None
+        self.portfolio = Portfolio()
 
         # Reset values
-        self.lbl_header_value["text"] = "0"
+        #self.lbl_header_value["text"] = "0"
+        self.cost.set(str(self.portfolio.value))
+
+        for lbl_position in self.positions:
+            lbl_position.destroy()
 
         #Remove close button
         self.btn_reset.grid_remove()
@@ -83,10 +97,16 @@ class StockTrackerGui:
             
         self.portfolio = Portfolio(filepath, 'degiro')
 
-        #window_frames = self.master.winfo_children()
-        #print(window_frames)
-        self.lbl_header_value["text"] = str(self.portfolio.value)
+        self.cost.set(str(self.portfolio.value))
 
+        i = 0
+        for key in self.portfolio.positions.keys():
+            lbl_position = tk.Label(self.frm_body_main, text=str(self.portfolio.positions[key]), bg=self.clr_accent_1)
+            lbl_position.grid(row=i, column=0, sticky="nsew", padx=2, pady=2)
+            self.frm_body_main.rowconfigure(i, weight=1)
+            self.positions.append(lbl_position)
+            i += 1
+            
         #Add close portfolio button
         self.btn_reset.grid()
 
